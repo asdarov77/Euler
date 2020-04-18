88ee84932ad231a61d2ecca88e83f1842e733cec
asdarov77 <asdarov77@mail.ru>
2020-04-18 19:01:08 +0300

    'changes'

--- 30_Пятые_степени_цифр.py
+++ 30_Пятые_степени_цифр.py
@@ -2,6 +2,25 @@
 """
 Created on Thu Apr 16 23:24:42 2020
 
-@author: Admin
-"""
+https://euler.jakumo.org/problems/view/30.html
 
+"""
+sum=0
+    
+def function(a):
+    mas = 0
+    number=a
+    while a > 0:
+        mas+=(a % 10)**5
+        a = a // 10
+    if number==mas:
+        return 1           
+
+
+
+for number in range(100,10000000):
+        if function(number)==1:
+            print(number)
+            sum+=number
+print(sum)
+        
